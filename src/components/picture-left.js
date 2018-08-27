cf7edc16c6f6ab2727dae50b26d7d7db2c70b37b
vercamach <machackovaver@gmail.com>
2018-08-27 13:53:09 +0200

    d

--- src/components/picture-left.js
+++ src/components/picture-left.js
@@ -103,7 +103,7 @@ const pictureLeft = props => {
             }}
           >
             {' '}
-            <img src={props.image} />
+            <img src={props.image} alt={props.image_alt} />
           </div>
         </section>
       )}
