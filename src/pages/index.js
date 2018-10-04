752f47c4a49449ba9c535b2b7d3d5e48094c7d34
vercamach <machackovaver@gmail.com>
2018-10-04 15:02:02 +0200

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ class Index extends React.Component {
   changeDirectory() {
     if (typeof window !== 'undefined') {
       let userLang = window.navigator.language || window.navigator.userLanguage
-      if (userLang === 'da-DA') {
+      if (userLang === 'da-DK') {
         window.location.href = '/da/'
       } else {
         window.location.href = '/en/'
